6f1932aaa1d216ea90c9c7e89b69cef15d6308b1
Nat Garrido <natgramirez@gmail.com>
2017-04-20 09:44:14 -0300

    de mayor igual a mayor

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,11 +1,11 @@
 /*
 Escriba una función que reciba un arreglo de n posiciones, en cada una de estas posiciones está una
-edad, debe retornar los años de nacimiento de sólo aquellas edades que sean mayores de 18 los números pares
+edad, debe retornar los años de nacimiento de sólo aquellas edades que sean MAYORES de 18 los números pares
 [20,16,89,6] -> [1997, 1928]
 */
 
 
-var edades = [20, 16, 89, 6, 21, 18, 22];
+var edades = [20, 16, 89, 6, 21, 18, 22, 24];
 
 function edadPar(n) {
     if (n % 2 == 0) {
@@ -15,7 +15,7 @@ function edadPar(n) {
 
 function mayorEdad(fechNac) {
     //en caso de que la persona naciera a lo mas en 1999 se trataria de un mayor de edad    
-    if (fechNac <= 1999) {
+    if (fechNac < 1999) {
         return true;
     } else {
         return false;
